6ef9a251c1f9fdbdad92121a37e2950e675c7b7b
Thomas deBlaquiere <tdwebdesignmsu@yahoo.com>
2022-12-02 11:40:52 -0500

    Created models and registered them to admin

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+    name = models.CharField(max_length=255)
+    directions = models.TextField()
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.DecimalField(decimal_places=2, default=1.00)
+    measure = models.TextField(max_length=255)
+
+
